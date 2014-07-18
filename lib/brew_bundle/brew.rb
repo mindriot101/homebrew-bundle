c90c7e4cf7b3abfbccae93742c2b87982fc558be
Simon Walker <s.r.walker101@googlemail.com>
2014-07-18 18:56:23 +0100

    Adding simple test run commands

--- lib/brew_bundle/brew.rb
+++ lib/brew_bundle/brew.rb
@@ -1,6 +1,19 @@
 class Brew
+
+  @@stub_run = false
+
   def self.run_command(command)
-    p "brew #{command}"
+    if @@stub_run
+      p "brew #{command}"
+    end
+  end
+
+  def self.configure(&block)
+    yield self
+  end
+
+  def self.test_run=(value)
+    @@stub_run = value
   end
 end
 
